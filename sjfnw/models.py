96ed9d8c9ec4f60ce9933210967d9fb74a12aa6d
Anna Luisa Patiño West <anna.luisa.west@formidablelabs.com>
2016-12-17 17:24:37 -0800

    minor cleanup

--- sjfnw/models.py
+++ sjfnw/models.py
@@ -1,16 +1,15 @@
 from django.contrib.auth.models import User
 from django.core.validators import MaxLengthValidator
 
-# pylint: disable=protected-access
+# hacky patch to give User username max length of 100 (instead of default 30)
 
-# User username length patch
+# pylint: disable=protected-access,unsubscriptable-object
 def patch_user_model(model):
   field = model._meta.get_field("username")
   field.max_length = 100
-  field.help_text = ('Required, 100 characters or fewer. Only letters, numbers,'
-                     ' and @, ., +, -, or _ characters.')
+  field.help_text = field.help_text.replace('30', '100')
 
-  # patch because validator doesn't change if we change max_length
+  # manually update field's validator
   for validator in field.validators:
     if isinstance(validator, MaxLengthValidator):
       validator.limit_value = 100
@@ -18,23 +17,13 @@ def patch_user_model(model):
   # patch admin site forms
   from django.contrib.auth.forms import UserChangeForm, UserCreationForm, AuthenticationForm
 
-  # pylint: disable=unsubscriptable-object
   new_help_text = UserChangeForm.base_fields['username'].help_text.replace('30', '100')
 
-  UserChangeForm.base_fields['username'].max_length = 100
-  UserChangeForm.base_fields['username'].widget.attrs['maxlength'] = 100
-  UserChangeForm.base_fields['username'].validators[0].limit_value = 100
-  UserChangeForm.base_fields['username'].help_text = new_help_text
-
-  UserCreationForm.base_fields['username'].max_length = 100
-  UserCreationForm.base_fields['username'].widget.attrs['maxlength'] = 100
-  UserCreationForm.base_fields['username'].validators[0].limit_value = 100
-  UserCreationForm.base_fields['username'].help_text = new_help_text
-
-  AuthenticationForm.base_fields['username'].max_length = 100
-  AuthenticationForm.base_fields['username'].widget.attrs['maxlength'] = 100
-  AuthenticationForm.base_fields['username'].validators[0].limit_value = 100
-  AuthenticationForm.base_fields['username'].help_text = new_help_text
+  for form in [UserChangeForm, UserCreationForm, AuthenticationForm]:
+    form.base_fields['username'].max_length = 100
+    form.base_fields['username'].widget.attrs['maxlength'] = 100
+    form.base_fields['username'].validators[0].limit_value = 100
+    form.base_fields['username'].help_text = new_help_text
 
 if User._meta.get_field("username").max_length != 100:
   patch_user_model(User)
