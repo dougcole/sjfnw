43514b9583852c72e69d5b8192e80b6e29411af6
Anna Luisa Patiño West <aisapatino@gmail.com>
2017-02-05 09:57:34 -0800

    put site into maintenance

--- sjfnw/settings.py
+++ sjfnw/settings.py
@@ -99,7 +99,7 @@ FILE_UPLOAD_HANDLERS = ('sjfnw.grants.storage.BlobstoreFileUploadHandler',)
 TEST_RUNNER = 'sjfnw.tests.base.ColorTestSuiteRunner'
 
 # Determines whether site is in maintenance mode. See urls.py
-MAINTENANCE = False
+MAINTENANCE = True
 # Date and/or time when site is expected to be out of maintenance mode.
 # See maintenance.html. For display only.
-MAINTENANCE_END_DISPLAY = ''
+MAINTENANCE_END_DISPLAY = 'Sunday February 5 at 9:45am PST'
