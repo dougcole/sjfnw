339c7f0d349a5072c3fa27bca7a7a79775ae5e17
Anna Luisa Patiño West <aisapatino@gmail.com>
2017-02-05 09:57:36 -0800

    update admin site

--- sjfnw/admin.py
+++ sjfnw/admin.py
@@ -3,7 +3,9 @@ import logging
 from django.contrib import admin, messages
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import Group, User
+from django.core.urlresolvers import reverse
 
+from sjfnw import utils
 from sjfnw.fund.models import Member
 from sjfnw.grants.models import Organization
 
@@ -13,39 +15,34 @@ logger = logging.getLogger('sjfnw')
 # ---------------------
 
 class BaseModelAdmin(admin.ModelAdmin):
-  """ Base class for setting a universal per-page limit """
+  """ Provide baseline per-page limit to reduce page load times """
   list_per_page = 30
 
 
 class BaseShowInline(admin.TabularInline):
-  """ Base inline for read only items """
+  """ For read only items """
   extra = 0
   max_num = 0
   can_delete = False
 
 
 class YearFilter(admin.SimpleListFilter):
-  """ Base filter by year. Usage: create a child class and override the class variables:
+  """ Filter results by the year on the given date field.
 
-    Required override:
-      filter_model: the model that has the date field to use for filtering
-
-    Optional overrides:
-      intermediate: string that connects the model being filtered to the filter_model
-                    (required if the model being filtered != the filter_model)
-      title: displayed in filters sidebar (default: 'year')
-      parameter_name: query parameter name (default: 'year')
+    Date field does not have to be on the model being filtered
+    To use, create a child class and override the class variables below.
   """
+  filter_model = None   # model being filtered
+  field = ''            # name of date field used to filter
+  intermediate = ''     # string path to the model that 'field' is on - if not filter_model
+
   title = 'year'
   parameter_name = 'year'
-  filter_model = None
-  field = ''
-  intermediate = ''
 
   def lookups(self, request, model_admin):
-    dates = (self.filter_model.objects
-        .values_list(self.field, flat=True)
-        .order_by('-%s' % self.field))
+    """ Returns an array of year values found in existing data """
+    dates = (self.filter_model.objects.values_list(self.field, flat=True)
+                                       .order_by('-%s' % self.field))
     prev = None
     years = []
     for date in dates:
@@ -71,6 +68,7 @@ class YearFilter(admin.SimpleListFilter):
       filt['{}{}__year'.format(key, self.field)] = year
       return queryset.filter(**filt)
 
+
 # Configure admin site
 # ---------------------
 
@@ -82,29 +80,47 @@ admin.site.index_title = None
 admin.site.unregister(Group)
 admin.site.unregister(User)
 
-class MemberI(BaseShowInline):
-  model = Member
-  fields = ('first_name', 'last_name')
-  readonly_fields = ('first_name', 'last_name')
-  show_change_link = True
-
 class UserA(UserAdmin):
-  """ Customized ModelAdmin using django's UserAdmin as base"""
   list_display = ('username', 'is_superuser')
   search_fields = ('username',)
   fieldsets = (
       (None, {
         'fields': (
-          ('username', 'password'),
-          ('date_joined', 'last_login')
+          ('username', 'change_password'),
+          ('member_link', 'organization_link')
         )
       }),
-      ('Permissions', {
-        'fields': ('is_active', 'is_staff', 'is_superuser')
+      ('Details', {
+        'classes': ('collapse',),
+        'fields': (
+          ('date_joined', 'last_login'),
+          ('is_staff', 'is_superuser'),
+          'is_active'
+        )
       })
   )
-  readonly_fields = ['last_login', 'date_joined']
+  readonly_fields = ('last_login', 'date_joined', 'change_password',
+                     'member_link', 'organization_link')
 
-  inlines = (MemberI,)
+  def member_link(self, obj):
+    if hasattr(obj, 'member'):
+      return utils.admin_change_link('fund_member', obj.member, new_tab=True)
+    return '-'
+  member_link.allow_tags = True
+  member_link.short_description = 'Member'
+
+  def organization_link(self, obj):
+    if hasattr(obj, 'organization'):
+      return utils.admin_change_link('grants_organization', obj.organization, new_tab=True)
+    return '-'
+  organization_link.allow_tags = True
+  organization_link.short_description = 'Organization'
+
+  def change_password(self, obj):
+    return utils.create_link(reverse('admin:auth_user_password_change', args=(obj.pk,)),
+                             'Change password')
+
+  change_password.allow_tags = True
+  change_password.short_description = 'Password'
 
 admin.site.register(User, UserA)
