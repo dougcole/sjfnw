3ad5886cd505ee29083441218849045864e111f2
Anna Luisa Patiño West <aisapatino@gmail.com>
2017-03-23 10:18:52 -0700

    update assert_draft_matches_app for new narratives

--- sjfnw/grants/tests/base.py
+++ sjfnw/grants/tests/base.py
@@ -18,7 +18,7 @@ class BaseGrantTestCase(BaseTestCase):
     self.org = factories.OrganizationWithProfile() if with_profile else factories.Organization()
     self.login_strict(self.org.user.username, 'password')
 
-  def assert_draft_matches_app(self, draft, app):
+  def assert_draft_matches_app(self, draft, app, app_narratives=None):
     """ Assert that app is a superset of draft
         (All draft fields match app, but app may have additional fields)
         Handles conversion of timeline format between the two. """
@@ -27,8 +27,11 @@ class BaseGrantTestCase(BaseTestCase):
     for field, value in draft_contents.iteritems():
       if hasattr(app, field):
         self.assertEqual(unicode(value), unicode(getattr(app, field)))
+      elif app_narratives:
+        self.assertEqual(value, app_narratives[field])
       else:
-        self.assertEqual(value, app.get_narrative_answer(field))
+        self.assertEqual(value, app.get_narrative_answer(field),
+            msg='Draft narrative answer for "{}" did not match app'.format(field))
     for field in models.GrantApplication.file_fields():
       if hasattr(draft, field):
         self.assertEqual(getattr(draft, field), getattr(app, field))
